68caba2cd7621e9f828eed45832132a8d1463bdb
LaPatusek <ddrrusek@gmail.com>
2024-04-29 18:23:56 +0200

    projects

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import propImg from '../Assets/dritf.jpg';
+import Portfolio from '../Assets/PortfolioWeb.webp';
 import Card from '../Components/UI/Card';
 import styles from './Projects.module.css';
 
@@ -16,66 +16,104 @@ const Projects: React.FC = () => {
       </div>
       <div className={`${styles.projects} grid`}>
         <div className={styles.box}>
-          <img src={propImg} alt='' />
+          <img
+            src={
+              'https://api.apify.com/v2/key-value-stores/GPZF9d2xRgnlMMttJ/records/www.agmitransport.pl-scroll_lossy-comp'
+            }
+            alt=''
+          />
           <div className={styles['box-text']}>
             <h3>AGMI TRANSPORT</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p>Strona profesjonalnej firmy logistycznej</p>
           </div>
         </div>
 
         <div className={styles.box}>
-          <img src={propImg} alt='' />
+          <img
+            src={
+              'https://api.apify.com/v2/key-value-stores/ecvWqYJqvoilY9H5L/records/devcol.vercel.app-scroll_lossy-comp'
+            }
+            alt='City of Lights'
+          />
           <div className={styles['box-text']}>
             <h3>City of Lights</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p>Strona przeznaczona serwerowi GTA RP</p>
           </div>
         </div>
 
         <div className={styles.box}>
-          <img src={propImg} alt='' />
+          <img
+            src={
+              '	https://api.apify.com/v2/key-value-stores/eIzQy5YZs8MvzSfAE/records/stronynawypasie.pl-scroll_lossy-comp'
+            }
+            alt='Strony na Wypasie'
+          />
           <div className={styles['box-text']}>
             <h3>Strony na Wypasie</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p>
+              Strona firmy, z którą współpracowałem. Zajmująca się tworzeniem
+              stron internetowych
+            </p>
           </div>
         </div>
 
         <div className={styles.box}>
-          <img src={propImg} alt='' />
+          <img
+            src={
+              'https://api.apify.com/v2/key-value-stores/G0KLIkLGv7A73VKeR/records/www.excelnawypasie.pl-scroll_lossy-comp'
+            }
+            alt='Excel na Wypasie'
+          />
           <div className={styles['box-text']}>
             <h3>Excel na Wypasie</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p>Kursy Excel Online</p>
           </div>
         </div>
 
         <div className={styles.box}>
-          <img src={propImg} alt='' />
+          <img
+            src={
+              'https://api.apify.com/v2/key-value-stores/1NGmhcail7Q89XQ61/records/selenbit.pl-scroll_lossy-comp'
+            }
+            alt='SelenBit'
+          />
           <div className={styles['box-text']}>
             <h3>SELENBIT</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p>Profesjonalne szkolenia na terenie Podkarpacia</p>
           </div>
         </div>
 
         <div className={styles.box}>
-          <img src={propImg} alt='' />
+          <img src={Portfolio} alt='Portfolio' />
           <div className={styles['box-text']}>
             <h3>Portfolio</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p>Strona, na której obecnie jesteśmy</p>
           </div>
         </div>
 
         <div className={styles.box}>
-          <img src={propImg} alt='' />
+          <img
+            src={
+              'https://api.apify.com/v2/key-value-stores/DVwBtoJJShIGkeqkj/records/www.excelrzeszow.pl-scroll_lossy-comp'
+            }
+            alt='Excel Rzeszów'
+          />
           <div className={styles['box-text']}>
             <h3>Excel Rzeszów</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p>Szkolenia Excel na terenie Rzeszowa</p>
           </div>
         </div>
 
         <div className={styles.box}>
-          <img src={propImg} alt='' />
+          <img
+            src={
+              'https://api.apify.com/v2/key-value-stores/qkR4Eg3GxSa1dheyk/records/barber-eta-bice.vercel.app-scroll_lossy-comp'
+            }
+            alt='Zakład Fryzjerski'
+          />
           <div className={styles['box-text']}>
             <h3>Zakład Fryzjerski</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p>Projekt w trakcie produkcji</p>
           </div>
         </div>
       </div>
